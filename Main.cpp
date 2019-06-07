9185ab17609d1054efe09eeedee43487a7904494
Roselynn Conrady <conrady.roselynn@gmail.com>
2019-06-07 16:31:43 -0500

    done!

--- Main.cpp
+++ Main.cpp
@@ -5,21 +5,27 @@
 #include <iostream>
 #include "Shape.h"
 #include "Rectangle.h"
+#include "Circle.h"
+#include "Triangle.h"
 #include <string>
 
 float w;
 float l;
+float r;
+float b;
 std::string color;
 int choice;
 
 int main() {
 	std::cout << "1. Rectangle" << std::endl;
-	//std::cout << "2. Circle" << std::endl;
-	//std::cout << "3. Triangle" << std::endl;
+	std::cout << "2. Circle" << std::endl;
+	std::cout << "3. Triangle" << std::endl;
 	std::cin >> choice;
 
 	switch (choice) {
+		
 	case 1:
+	{
 		std::cout << "Width?" << std::endl;
 		std::cin >> w;
 		std::cout << "Length?" << std::endl;
@@ -32,9 +38,36 @@ int main() {
 		Rectangle someColor();
 		std::cout << aRec.returnColor() << std::endl;
 		break;
+	}
 
+	case 2:
+	{
+		std::cout << "Radius?" << std::endl;
+		std::cin >> r;
 
-		//case 2:
+		Circle aCirc(r);
+		std::cout << "Area: " << aCirc.returnArea() << std::endl;
+
+		std::cout << "Here's a color!" << std::endl;
+		Circle someColor();
+		std::cout << aCirc.returnColor() << std::endl;
+	break;
+	}
+	case 3:
+
+		std::cout << "Base?" << std::endl;
+		std::cin >> b;
+		std::cout << "Length?" << std::endl;
+		std::cin >> l;
+
+		Triangle aTri(b, l);
+		std::cout << "Area: " << aTri.returnArea() << std::endl;
+
+		std::cout << "Here's a color!" << std::endl;
+		Triangle someColor();
+		std::cout << aTri.returnColor() << std::endl;
+		break;
+
 	}
 
 	system("PAUSE");
